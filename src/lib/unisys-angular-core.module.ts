11d9570372bfb67531a8ce66b3a21d60bade11d2
jakub.bajzath <bajzath.jakub@gmail.com>
2019-01-24 10:40:50 +0100

    0.1.9

--- src/lib/unisys-angular-core.module.ts
+++ src/lib/unisys-angular-core.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FooterComponent} from './footer/footer.component';
 import {HeaderComponent} from './header/header.component';
-import {AppRoutingModule} from './modules/app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 import {NgxPermissionsModule} from 'ngx-permissions';
 import {UserProfileComponent} from './header/user-profile/user-profile.component';
 import {AppSettingsComponent} from './header/app-settings/app-settings.component';
