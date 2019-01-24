5303af7acd8fd8fcb0fc20ef6f66bdcc6f3dee38
jakub.bajzath <bajzath.jakub@gmail.com>
2019-01-24 10:00:26 +0100

    0.1.2

---
+++ src/lib/default/default.component.ts
@@ -0,0 +1,23 @@
+import {Component, ElementRef, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {MenuItem} from '../models';
+
+@Component({
+  selector: 'app-default',
+  templateUrl: './default.component.html',
+  styleUrls: ['./default.component.css']
+})
+export class DefaultComponent implements OnInit {
+  public context;
+  public item;
+
+  constructor() {
+    this.context = this;
+  }
+
+  ngOnInit() {
+  }
+
+  public mainMenu: MenuItem[] = [
+    {routerLink: ['/default'], translation: 'STATS', icon: ''},
+  ];
+}
---
+++ src/lib/default/default.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DefaultComponent} from './default.component';
+import {DefaultRoutingModule} from './default-routing.module';
+import {SharedModule} from '../shared/shared.module';
+import {ReactiveFormsModule} from '@angular/forms';
+import {NgxPermissionsModule} from 'ngx-permissions';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DefaultRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+    NgxPermissionsModule.forChild(),
+  ],
+  declarations: [
+    DefaultComponent,
+  ],
+  entryComponents: [],
+})
+export class DefaultModule {
+}
