2d8dba369a9bcb5500401f1217ffec39282e29ba
jakub.bajzath <bajzath.jakub@gmail.com>
2019-01-24 10:47:32 +0100

    0.1.10

--- src/lib/aside/aside.component.ts
+++ src/lib/aside/aside.component.ts
@@ -9,13 +9,13 @@ import {MenuItem} from '../models';
 export class AsideComponent implements OnInit {
   public mainMenuList: MenuItem[] = [
     {routerLink: ['default'], permission: 'default', translation: 'STATS', icon: 'fa fa-tasks'},
-    {routerLink: ['expense'], permission: 'expense', translation: 'EXPENSES', icon: 'fa fa-file-text-o'},
-    {routerLink: ['transaction'], permission: 'transaction', translation: 'TRANSACTIONS', icon: 'fa fa-bars'},
-    {routerLink: ['draw'], permission: 'draw', translation: 'DRAWS', icon: 'fa fa-bank'},
-    {routerLink: ['cofinance'], permission: 'cofinance', translation: 'COFINANCE', icon: 'fa fa-life-ring'},
-    {routerLink: ['contact'], permission: 'contact', translation: 'PARTNERS', icon: 'fa fa-suitcase'},
-    {routerLink: ['grant'], permission: 'grant', translation: 'GRANTS', icon: 'fa fa-sign-in'},
-    {routerLink: ['project'], permission: 'project', translation: 'PROJECTS', icon: 'fa fa-cubes'}
+    // {routerLink: ['expense'], permission: 'expense', translation: 'EXPENSES', icon: 'fa fa-file-text-o'},
+    // {routerLink: ['transaction'], permission: 'transaction', translation: 'TRANSACTIONS', icon: 'fa fa-bars'},
+    // {routerLink: ['draw'], permission: 'draw', translation: 'DRAWS', icon: 'fa fa-bank'},
+    // {routerLink: ['cofinance'], permission: 'cofinance', translation: 'COFINANCE', icon: 'fa fa-life-ring'},
+    // {routerLink: ['contact'], permission: 'contact', translation: 'PARTNERS', icon: 'fa fa-suitcase'},
+    // {routerLink: ['grant'], permission: 'grant', translation: 'GRANTS', icon: 'fa fa-sign-in'},
+    // {routerLink: ['project'], permission: 'project', translation: 'PROJECTS', icon: 'fa fa-cubes'}
   ];
 
   constructor() {
