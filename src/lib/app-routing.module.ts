50035f9af396370ade1590b61de444489a129b49
jakub.bajzath <bajzath.jakub@gmail.com>
2019-01-29 13:19:27 +0100

    0.9.1

--- src/lib/app-routing.module.ts
+++ src/lib/app-routing.module.ts
@@ -1,26 +1,16 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {AuthGuard} from './services/auth-guard.service';
-import {DefaultModule} from './default/default.module';
-import {SettingsModule} from './admin/settings/settings.module';
-import {UsersModule} from './admin/users/users.module';
-import {BankAccountModule} from './admin/bank-account/bank-account.module';
-
-// Workaround for lazy load modules
-export function proxyDefaultModule() {return DefaultModule;}
-export function proxyAdminBankAccountModule(){return BankAccountModule;}
-export function proxySettingsModule(){return SettingsModule;}
-export function proxyUsersModule(){return UsersModule};
 
 const appRoutes: Routes = [
   {path: '', redirectTo: 'default', pathMatch: 'full'},
-  {path: 'default', loadChildren: proxyDefaultModule},
-  {path: 'admin/bank-account', loadChildren: proxyAdminBankAccountModule},
+  {path: 'default', loadChildren: './default/default.module#DefaultModule'},
+  {path: 'admin/bank-account', loadChildren: './admin/bank-account/bank-account.module#BankAccountModule'},
   // {path: 'admin/category', loadChildren: './admin/category/category.module#CategoryModule'},
   // {path: 'admin/roles', loadChildren: './admin/role/role.module#RoleModule'},
-  {path: 'admin/settings', loadChildren: proxySettingsModule},
+  {path: 'admin/settings', loadChildren: './admin/settings/settings.module#SettingsModule'},
   // {path: 'admin/tags', loadChildren: './admin/tags/tags.module#TagsModule'},
-  {path: 'admin/user', loadChildren: proxyUsersModule},
+  {path: 'admin/user', loadChildren: './admin/users/users.module#UsersModule'},
   // {path: 'contact', loadChildren: './contact/contact.module#ContactModule'},
   // {path: 'draw', loadChildren: './draw/draw.module#DrawModule'},
   // {path: 'expense', loadChildren: './expense/expense.module#ExpenseModule'},
