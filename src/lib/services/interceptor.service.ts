94b35e745c8b57f6da616f0f3fd52e01832526ed
Michal Stupak <michal@unite.sk>
2019-02-26 13:29:57 +0100

    odstranenie vypisovanie toasteru pri PUT+POST+DELETE

--- src/lib/services/interceptor.service.ts
+++ src/lib/services/interceptor.service.ts
@@ -18,42 +18,28 @@ export class InterceptorService implements HttpInterceptor {
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-        // ZMENIM SYSTEM PRIDAVANIA HLAVICKY
-
-        // const token: string = localStorage.getItem('token');
-
-        // if (token) {
-        //     request = request.clone({ headers: request.headers.set('Authorization', 'Bearer ' + token) });
-        // }
-        //
-        // if (!request.headers.has('Content-Type')) {
-        //     request = request.clone({ headers: request.headers.set('Content-Type', 'application/json') });
-        // }
-        //
-        // request = request.clone({ headers: request.headers.set('Accept', 'application/json') });
-
         this.appState.addRequest(request);
         return next.handle(request).pipe(
             map((event: HttpEvent<any>) => {
                 if (event instanceof HttpResponse) {
                     this.appState.removeRequest(request);
-                    switch(request.method) {
-                        case "PUT":
-                            this.translate.get('ITEM_HAS_BEEN_SAVED').subscribe((res: string) => {
-                                this.toastrService.success('',res);
-                            });
-                            break;
-                        case "POST":
-                            this.translate.get('ITEM_HAS_BEEN_CREATED').subscribe((res: string) => {
-                                this.toastrService.success('',res);
-                            });
-                            break;
-                        case "DELETE":
-                            this.translate.get('ITEM_HAS_BEEN_DELETED').subscribe((res: string) => {
-                                this.toastrService.error('',res);
-                            });
-
-                    }
+                    // switch(request.method) {
+                    //     case "PUT":
+                    //         this.translate.get('ITEM_HAS_BEEN_SAVED').subscribe((res: string) => {
+                    //             this.toastrService.success('',res);
+                    //         });
+                    //         break;
+                    //     case "POST":
+                    //         this.translate.get('ITEM_HAS_BEEN_CREATED').subscribe((res: string) => {
+                    //             this.toastrService.success('',res);
+                    //         });
+                    //         break;
+                    //     case "DELETE":
+                    //         this.translate.get('ITEM_HAS_BEEN_DELETED').subscribe((res: string) => {
+                    //             this.toastrService.error('',res);
+                    //         });
+                    //
+                    // }
                 }
                 return event;
             }),catchError((response: any) => {
