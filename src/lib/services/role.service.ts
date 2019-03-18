92c3aacc4cce207a4d12948b20003c1c64cebf31
Michal Stupak <michal@unite.sk>
2019-03-18 17:33:08 +0100

    potrebujem este role od vladina

--- src/lib/services/role.service.ts
+++ src/lib/services/role.service.ts
@@ -1,15 +1,17 @@
 import {Injectable} from '@angular/core';
-
-import {BaseService} from './base.service';
 import {Subject} from 'rxjs';
+import { BaseApolloService } from './baseApollo.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RoleService extends BaseService {
+export class RoleService extends BaseApolloService {
   protected url = 'role';
   public allPermissions;
   public permissionsChanged = new Subject();
+  protected selection = 'id, name, surname, username, email, roles {id, name}, frontend_permissions {id, name}';
+  protected operationType = 'role';
+  protected operationTypePlural = 'roles';
 
   getAllPermissions($id) {
     return this.http.get(this.url + '/' + $id).subscribe(data => {
