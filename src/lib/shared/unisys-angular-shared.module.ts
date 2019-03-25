2d4df42fc6237914df010d1bd54d31c759b66384
jakub.bajzath <bajzath.jakub@gmail.com>
2019-03-25 14:25:11 +0100

    ZFS-88

--- src/lib/shared/unisys-angular-shared.module.ts
+++ src/lib/shared/unisys-angular-shared.module.ts
@@ -1,33 +1,34 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {TranslateLoader, TranslateModule, TranslateService} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {RouterModule} from '@angular/router';
-import {DropdownDirective} from './dropdown.directive';
-import {FilterColumnsPipe} from './filter-columns.pipe';
-import {FilterVisibilityPipe} from './filter-visibility.pipe';
-import {SlideAssetDirective} from './slide-asset.directive';
-import {SortByPipe} from './sort-by.pipe';
-import {NgSelectModule} from '@ng-select/ng-select';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {TableComponent} from './table/table.component';
-import {ModalDeleteComponent} from './table/modal-delete/modal-delete.component';
-import {ModalTagComponent} from './table/modal-tag/modal-tag.component';
-import {ModalTagDeleteComponent} from './table/modal-tag-delete/modal-tag-delete.component';
-import {NgxPermissionsModule} from 'ngx-permissions';
-import {BsDatepickerModule, BsLocaleService, TooltipModule} from 'ngx-bootstrap';
-import {TruncateModule} from 'ng2-truncate';
-import {defineLocale} from 'ngx-bootstrap/chronos';
-import {skLocale} from 'ngx-bootstrap/locale';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { RouterModule } from '@angular/router';
+import { DropdownDirective } from './dropdown.directive';
+import { FilterColumnsPipe } from './filter-columns.pipe';
+import { FilterVisibilityPipe } from './filter-visibility.pipe';
+import { SlideAssetDirective } from './slide-asset.directive';
+import { SortByPipe } from './sort-by.pipe';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TableComponent } from './table/table.component';
+import { ModalDeleteComponent } from './table/modal-delete/modal-delete.component';
+import { ModalTagComponent } from './table/modal-tag/modal-tag.component';
+import { ModalTagDeleteComponent } from './table/modal-tag-delete/modal-tag-delete.component';
+import { NgxPermissionsModule } from 'ngx-permissions';
+import { BsDatepickerModule, BsLocaleService, TooltipModule } from 'ngx-bootstrap';
+import { TruncateModule } from 'ng2-truncate';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { skLocale } from 'ngx-bootstrap/locale';
 import { AngularEditorModule } from '@kolkov/angular-editor';
 
 // UniSys Modules
-import {UnisysAngularTabRouterModule} from '@weareunite/unisys-angular-tab-router';
-import {UnisysAngularFormGroupModule} from '@weareunite/unisys-angular-form-group';
-import {UnisysAngularInputWrapperModule} from '@weareunite/unisys-angular-input-wrapper';
-import {UnisysAngularViewButtonModule} from '@weareunite/unisys-angular-view-button';
-import {UnisysAngularProgressBarModule} from '@weareunite/unisys-angular-progress-bar';
+// import {UnisysAngularTabRouterModule} from '@weareunite/unisys-angular-tab-router';
+import { UnisysAngularTabRouterModule } from '../../../../unisys-angular-tab-router/src/lib/unisys-angular-tab-router.module';
+import { UnisysAngularFormGroupModule } from '@weareunite/unisys-angular-form-group';
+import { UnisysAngularInputWrapperModule } from '@weareunite/unisys-angular-input-wrapper';
+import { UnisysAngularViewButtonModule } from '@weareunite/unisys-angular-view-button';
+import { UnisysAngularProgressBarModule } from '@weareunite/unisys-angular-progress-bar';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -103,7 +104,7 @@ export class UnisysAngularSharedModule {
     translate: TranslateService,
     private localeService: BsLocaleService
   ) {
-    defineLocale('sk', skLocale)
+    defineLocale('sk', skLocale);
     this.localeService.use('sk');
 // userLang = translate.getBrowserLang(); for future translations
     translate.setDefaultLang('sk');
