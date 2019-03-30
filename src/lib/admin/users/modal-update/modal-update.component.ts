50c39036373ef047582454d3850177b14804eba3
jakub.bajzath <bajzath.jakub@gmail.com>
2019-03-30 10:30:05 +0100

    Delete button on products

--- src/lib/admin/users/modal-update/modal-update.component.ts
+++ src/lib/admin/users/modal-update/modal-update.component.ts
@@ -1,108 +1,108 @@
-import {Component, OnInit} from '@angular/core';
-import {User} from '../../../models';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import {BsModalRef} from 'ngx-bootstrap';
-import {Subscription} from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../../models';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { BsModalRef } from 'ngx-bootstrap';
+import { Subscription } from 'rxjs';
 import { RoleService } from '../../../services/role.service';
 
 @Component({
-  selector: 'app-modal-update',
-  templateUrl: './modal-update.component.html',
-  styleUrls: ['./modal-update.component.css']
+    selector: 'app-modal-update',
+    templateUrl: './modal-update.component.html',
+    styleUrls: ['./modal-update.component.css']
 })
 export class ModalUpdateComponent implements OnInit {
 
-  public item: User;
-  public service;
-  public roleList: any[];
-  public roleSubscription: Subscription;
+    public item: User;
+    public service;
+    public roleList: any[];
+    public roleSubscription: Subscription;
 
-  defaultForm = new FormGroup({
-    id: new FormControl(),
-    name: new FormControl(),
-    surname: new FormControl(),
-    username: new FormControl(),
-    email: new FormControl(),
-    password: new FormControl(),
-    password_confirmation: new FormControl(),
-    roles: new FormControl(),
-  });
+    defaultForm = new FormGroup({
+        id: new FormControl(),
+        name: new FormControl(),
+        surname: new FormControl(),
+        username: new FormControl(),
+        email: new FormControl(),
+        password: new FormControl(),
+        password_confirmation: new FormControl(),
+        roles: new FormControl(),
+    });
 
-  constructor(
-    public bsModalRef: BsModalRef,
-    public formBuilder: FormBuilder,
-    public roleService: RoleService,
-  ) {
-  }
+    constructor(
+        public bsModalRef: BsModalRef,
+        public formBuilder: FormBuilder,
+        public roleService: RoleService,
+    ) {
+    }
 
-  ngOnInit() {
-    this.builForm();
-    this.roleSubscription = this.roleService.listChanged
-      .subscribe(
-        (list) => {
-          this.roleList = list;
+    ngOnInit() {
+        this.builForm();
+        this.roleSubscription = this.roleService.listChanged
+            .subscribe(
+                (list) => {
+                    this.roleList = list;
+                }
+            );
+        this.roleService.getItemList();
+    }
+
+    private builForm() {
+        if (this.item) {
+            this.defaultForm = this.formBuilder.group({
+                id: [this.item.id],
+                name: [this.item.name, Validators.required],
+                surname: [this.item.surname, Validators.required],
+                username: [this.item.username, Validators.required],
+                email: [this.item.email, Validators.required],
+                password: [''],
+                password_confirmation: [''],
+                roles: [this.item.roles, Validators.required],
+                active: [this.item.active]
+            });
+        } else {
+            this.defaultForm = this.formBuilder.group({
+                id: '',
+                name: ['', Validators.required],
+                surname: ['', Validators.required],
+                username: ['', Validators.required],
+                email: ['', Validators.required],
+                password: ['', Validators.required],
+                password_confirmation: ['', Validators.required],
+                roles: ['', Validators.required],
+                active: [true]
+            });
         }
-      );
-    this.roleService.getItemList();
-  }
-
-  private builForm() {
-    if (this.item) {
-      this.defaultForm = this.formBuilder.group({
-        id: [this.item.id],
-        name: [this.item.name, Validators.required],
-        surname: [this.item.surname, Validators.required],
-        username: [this.item.username, Validators.required],
-        email: [this.item.email, Validators.required],
-        password: [''],
-        password_confirmation: [''],
-        roles: [this.item.roles, Validators.required],
-        active: [this.item.active]
-      });
-    } else {
-      this.defaultForm = this.formBuilder.group({
-        id: '',
-        name: ['', Validators.required],
-        surname: ['', Validators.required],
-        username: ['', Validators.required],
-        email: ['', Validators.required],
-        password: ['', Validators.required],
-        password_confirmation: ['', Validators.required],
-        roles: ['', Validators.required],
-        active: [true]
-      });
     }
-  }
 
-  public onSubmit() {
-    if (this.item) {
-      this.update();
-    } else {
-      this.create();
+    public onSubmit() {
+        if (this.item) {
+            this.update();
+        } else {
+            this.create();
+        }
+        const listSubscription = this.service.listChanged
+            .subscribe(
+                (listChanged) => {
+                    this.bsModalRef.hide();
+                    listSubscription.unsubscribe();
+                }
+            );
     }
-    const listSubscription = this.service.listChanged
-        .subscribe(
-            (listChanged) => {
-              this.bsModalRef.hide();
-              listSubscription.unsubscribe();
-            }
-        );
-  }
 
-  public update() {
-    this.service.updateUserFromList(this.defaultForm.value);
-  }
+    public update() {
+        this.service.updateUserFromList(this.defaultForm.value);
+    }
 
-  public create() {
-    const itemSubscription = this.service.itemChanged
-        .subscribe(
-            (item) => {
-              this.item = item;
-              this.builForm();
-              itemSubscription.unsubscribe();
-            }
-        );
-    this.service.pushUserToList(this.defaultForm.value);
-  }
+    public create() {
+        const itemSubscription = this.service.itemChanged
+            .subscribe(
+                (item) => {
+                    this.item = item;
+                    this.builForm();
+                    itemSubscription.unsubscribe();
+                }
+            );
+        this.service.pushUserToList(this.defaultForm.value);
+    }
 
 }
