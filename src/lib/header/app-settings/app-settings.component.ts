2aa3fff2b5c77a4ab90c5720f1aa84cca7d70b53
jakub.bajzath <bajzath.jakub@gmail.com>
2019-05-17 11:01:04 +0200

    ZFS-184

--- src/lib/header/app-settings/app-settings.component.ts
+++ src/lib/header/app-settings/app-settings.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {MenuItem, Contact} from '../../models';
-import {SettingsService} from '../../services/settings.service';
-import {Subscription} from 'rxjs';
+import { MenuItem, Contact } from '../../models';
+import { SettingsService } from '../../services/settings.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-app-settings',
@@ -9,32 +9,34 @@ import {Subscription} from 'rxjs';
   styleUrls: ['./app-settings.component.css']
 })
 export class AppSettingsComponent implements OnInit {
-    public  company:Contact;
-    private settingsSubscription : Subscription;
+  public company: Contact;
+  private settingsSubscription: Subscription;
 
-    constructor(
-        private settingsService: SettingsService
-    ) { }
+  constructor(
+    private settingsService: SettingsService
+  ) {
+  }
 
-    ngOnInit() {
-        this.settingsSubscription = this.settingsService.companyChanged
-            .subscribe(
-                (item) => {
-                    this.company = item;
-                }
-            );
-        this.settingsService.getCompany();
-    }
+  ngOnInit() {
+    this.settingsSubscription = this.settingsService.companyChanged
+      .subscribe(
+        (item) => {
+          this.company = item;
+        }
+      );
+    this.settingsService.getCompany();
+  }
 
-    public companyMenuList : MenuItem[] = [];
+  public companyMenuList: MenuItem[] = [];
 
-    public toolsMenuList : MenuItem[] = [];
+  public toolsMenuList: MenuItem[] = [];
 
-    public appMenuList : MenuItem[] = [
-        {routerLink:['admin','user'], permission:'admin.users', translation:'USERS',icon:'fa fa-users'},
-        {routerLink:['admin','roles'], permission:'admin.role', translation:'ROLES', icon: 'fa fa-lock'},
-        {routerLink:['admin','help'], permission:'admin.help', translation:'HELP', icon: 'fa fa-question-circle'},
-        {routerLink:['admin','logs'], permission:'admin.log', translation:'LOGS', icon: 'fa fa-history'},
-        {routerLink:['admin','settings','app'], permission:'admin.settings' ,translation:'APP_SETTINGS',icon:'fa fa-cog'},
-    ];
+  public appMenuList: MenuItem[] = [
+    {routerLink: ['admin', 'user'], permission: 'admin.users', translation: 'USERS', icon: 'fa fa-users'},
+    {routerLink: ['admin', 'roles'], permission: 'admin.role', translation: 'ROLES', icon: 'fa fa-lock'},
+    {routerLink: ['admin', 'help'], permission: 'admin.help', translation: 'HELP', icon: 'fa fa-question-circle'},
+    {routerLink: ['admin', 'logs'], permission: 'admin.log', translation: 'LOGS', icon: 'fa fa-exclamation-circle'},
+    {routerLink: ['admin', 'backup'], permission: 'admin.backups', translation: 'BACKUPS', icon: 'fa fa-history'},
+    {routerLink: ['admin', 'settings', 'app'], permission: 'admin.settings', translation: 'APP_SETTINGS', icon: 'fa fa-cog'},
+  ];
 }
