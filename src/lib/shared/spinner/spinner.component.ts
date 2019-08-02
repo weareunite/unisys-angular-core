2891fb0ebee0f5f135a95e49e21e1a248c2f61ba
jakub.bajzath <bajzath.jakub@gmail.com>
2019-08-02 11:29:25 +0200

    Weekly planner should be working, lot of queries and mutation added

---
+++ src/lib/shared/spinner/spinner.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class SpinnerComponent implements OnInit {
+
+  @Input('size') size;
+  @Input('color') color;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/lib/shared/unisys-angular-shared.module.ts
+++ src/lib/shared/unisys-angular-shared.module.ts
@@ -24,7 +24,7 @@ import {AngularEditorModule} from '@kolkov/angular-editor';
 
 // UniSys Modules
 import {UnisysAngularTabRouterModule} from '@weareunite/unisys-angular-tab-router';
-import {UnisysAngularFormGroupModule} from '@weareunite/unisys-angular-form-group';
+// import {UnisysAngularFormGroupModule} from '@weareunite/unisys-angular-form-group';
 import {UnisysAngularInputWrapperModule} from '@weareunite/unisys-angular-input-wrapper';
 import {UnisysAngularViewButtonModule} from '@weareunite/unisys-angular-view-button';
 import {UnisysAngularProgressBarModule} from '@weareunite/unisys-angular-progress-bar';
@@ -32,9 +32,10 @@ import {LogViewComponent} from './log-view/log-view.component';
 import {LoaderComponent} from './loader/loader.component';
 import {DumpObjectPipe} from '../pipes/dump-object.pipe';
 import {DumpersonComponent} from './dumperson/dumperson.component';
+import {SpinnerComponent} from './spinner/spinner.component';
 
 // Local UniSys GIT modules in project (for development purposes only !)
-// import {UnisysAngularFormGroupModule} from '../../../../unisys-angular-form-group/src/lib/unisys-angular-form-group.module';
+import {UnisysAngularFormGroupModule} from '../../../../unisys-angular-form-group/src/lib/unisys-angular-form-group.module';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -80,6 +81,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     TableComponent,
     LogViewComponent,
     LoaderComponent,
+    SpinnerComponent,
     DumpObjectPipe,
     DumpersonComponent
   ],
@@ -106,6 +108,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     UnisysAngularViewButtonModule,
     LogViewComponent,
     LoaderComponent,
+    SpinnerComponent,
     DumpObjectPipe,
     DumpersonComponent
   ],
