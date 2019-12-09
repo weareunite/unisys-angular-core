21a9669e1d30b221941888444868afa1a9a4bb49
Jakub Bajzath <bajzath.jakub@gmail.com>
2019-12-09 14:29:51 +0100

    CM-79

--- src/lib/services/user.service.ts
+++ src/lib/services/user.service.ts
@@ -58,6 +58,7 @@ export class UserService extends BaseApolloService {
   loadProfile() {
     this.permissionsService.loadPermissions(this.getLocalPermissions());
     const operationType = 'profile';
+    console.log('profile');
 
     let apolloInstnc = this.apollo.setOperationName('query')
       .setOperationType(operationType)
