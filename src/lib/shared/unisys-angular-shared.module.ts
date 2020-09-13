346d859d6c3bdc78627aa2b51693cbb61756b267
Michal Stupak <michal@unite.sk>
2020-09-13 23:02:10 +0200

    hot fix, pre datepicker

--- src/lib/shared/unisys-angular-shared.module.ts
+++ src/lib/shared/unisys-angular-shared.module.ts
@@ -37,12 +37,12 @@ import {CharAtPipe} from '../pipes/char-at.pipe';
 import {SubstringPipe} from '../pipes/substring.pipe';
 import {IsNaNPipe} from '../pipes/isNaN.pipe';
 import { SanitizeHtmlPipe } from '../pipes/sanitizeHtml.pipe';
-import { UnisysAngularFormGroupModule } from '@weareunite/unisys-angular-form-group';
+// import { UnisysAngularFormGroupModule } from '@weareunite/unisys-angular-form-group';
 import { UnisysAngularAppStateServiceService } from '@weareunite/unisys-angular-app-state-service';
 import { CoreService } from '../services/core.service';
 
 // Local UniSys GIT modules in project (for development purposes only !)
-// import {UnisysAngularFormGroupModule} from '../../../../unisys-angular-form-group/src/lib/unisys-angular-form-group.module';
+import {UnisysAngularFormGroupModule} from '../../../../unisys-angular-form-group/src/lib/unisys-angular-form-group.module';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -140,6 +140,8 @@ export class UnisysAngularSharedModule {
     private appStateService: UnisysAngularAppStateServiceService,
     private coreService: CoreService,
     private translate: TranslateService,
+    private localeService: BsLocaleService
+
   ) {
     const langCode = this.appStateService.getAppState('language');
     const userLang = translate.getBrowserLang();
@@ -149,6 +151,11 @@ export class UnisysAngularSharedModule {
     } else if (userLang) {
       selectedLang = userLang;
     }
-    this.coreService.setTranslation(selectedLang);
+    // this.coreService.setTranslation(selectedLang);
+    defineLocale(selectedLang, skLocale);
+    this.localeService.use(selectedLang);
+    this.translate.setDefaultLang(selectedLang);
+    this.translate.use(selectedLang);
+
   }
 }
